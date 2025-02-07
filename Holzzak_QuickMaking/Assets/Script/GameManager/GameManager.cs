84acf5b8a84e4642a60bc49e19ff436293fb9421
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2025-02-07 21:35:34 +0900

    250207
    플레이어 생성 기능 추가, UI 관련 내용 및 데이터 추가

--- Holzzak_QuickMaking/Assets/Script/GameManager/GameManager.cs
+++ Holzzak_QuickMaking/Assets/Script/GameManager/GameManager.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
+
+    public int cost = 5;
+
+    public List<GameObject> unitList = new List<GameObject>();
+
+    public List<GameObject> playerUnitSpawnLocation = new List<GameObject>();
+
+    public GameObject enemyUnitSpawnLocation;
+
+    public Text costText;
 
     private void Awake()
     {
@@ -13,4 +24,49 @@ public class GameManager : MonoBehaviour
             instance = this;
         }
     }
+
+    private void Update()
+    {
+        costText.text = cost.ToString();
+    }
+
+    public void SpawnUnit(int Code)
+    {
+        if(cost > 0)
+        {
+            switch (Code)
+            {
+                case 0:
+                    Instantiate(unitList[Code], playerUnitSpawnLocation[0].transform.position, playerUnitSpawnLocation[0].transform.rotation);
+                    cost -= 1;
+                    break;
+
+                case 1:
+                    Instantiate(unitList[Code], playerUnitSpawnLocation[0].transform.position, playerUnitSpawnLocation[0].transform.rotation);
+                    break;
+
+                case 2:
+                    Instantiate(unitList[Code], playerUnitSpawnLocation[0].transform.position, playerUnitSpawnLocation[0].transform.rotation);
+                    break;
+
+                case 3:
+                    Instantiate(unitList[Code], playerUnitSpawnLocation[0].transform.position, playerUnitSpawnLocation[0].transform.rotation);
+                    break;
+
+                case 4:
+                    Instantiate(unitList[Code], playerUnitSpawnLocation[0].transform.position, playerUnitSpawnLocation[0].transform.rotation);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            Debug.Log("보유 코스트를 전부 사용했습니다.");
+        }
+
+        
+    }
+
 }
